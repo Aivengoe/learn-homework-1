1ec2b9deea3be64b999c5da105445aa93fe59aa9
Aivengoe Edev <aivengoe2@gmail.com>
2019-11-28 01:20:31 +0300

    Выполнил задание ephem_bot.py

--- ephem_bot.py
+++ ephem_bot.py
@@ -13,6 +13,9 @@
 
 """
 import logging
+import ephem
+from settings import tele_token, PROXY
+from datetime import datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -20,15 +23,6 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
 )
-
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
 
 
 def greet_user(bot, update):
@@ -43,12 +37,36 @@ def talk_to_me(bot, update):
     update.message.reply_text(user_text)
  
 
+def  planet_constellation(bot, update):
+    user_text = update.message.text
+    planet =  user_text.split()[1]
+    to_day = datetime.today()
+    data_day = "{year}/{month}/{day}".format(year = to_day.year, month = to_day.month, day = to_day.day) 
+    
+    planets_dict ={
+        'Mercury': ephem.Mercury(data_day),
+        'Venus': ephem.Venus(data_day),
+        'Mars': ephem.Mars(data_day),
+        'Jupiter': ephem.Jupiter(data_day),
+        'Saturn': ephem.Saturn(data_day),
+        'Uranus': ephem.Uranus(data_day),
+        'Neptune': ephem.Neptune(data_day),
+    }
+
+    if planet not in planets_dict.keys():
+        update.message.reply_text('Такой планеты рядом нет')
+    else:
+        constellation = ephem.constellation(planets_dict[planet])[1]
+        update.message.reply_text(f'Планета: {planet} \nCегодня в созвездии {constellation}')
+
+
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
+    mybot = Updater(tele_token, request_kwargs=PROXY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", planet_constellation))
     
     mybot.start_polling()
     mybot.idle()
