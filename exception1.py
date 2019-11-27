1da3892a4bcc874966ad25736be3b15ed6289f68
Aivengoe Edev <aivengoe2@gmail.com>
2019-11-27 22:46:23 +0300

    Выполнил задание exeption1.py

--- exception1.py
+++ exception1.py
@@ -10,11 +10,31 @@
     
 """
 
+question_dict = {
+    "0": "Zero",
+    "Как дела?": "Хорошо!",
+    "Что делаешь?": "Программирую",
+    "Ты откуда?": "Я везде, не оборачивайся",
+    "Знаешь Алису?": "Да, но мне больше синяя нравится.",
+    "Ответ на главный вопрос жизни вселенной и всего": "42",
+    "Где деньги?": "Спроси у Лебовски"
+}
+
+
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    while True:
+        try:
+            question = input("Задай вопрос - ")
+            if question in question_dict.keys():
+                print(question_dict[question])
+                continue
+            else:
+                print('Нет подходяшего вопроса, нет ответа')
+                continue
+        except KeyboardInterrupt:
+            print()
+            print('Пока!')
+            break
+        
 if __name__ == "__main__":
     ask_user()
